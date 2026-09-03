90361be9eba878a46a1d4fd9b7119f511874dad4
Ryen Burns <rburns@impactnetworking.com>
2026-09-03 13:42:43 -0500

    Added markup to text content to match demo site, but I'm still going to need to add Bootstrap containers, cols, and rows to get it to look right.

--- assignment-app/src/pages/Home.jsx
+++ assignment-app/src/pages/Home.jsx
@@ -3,26 +3,29 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Content goes here</p>
-      <p>
-        Sourced from this Kaggle Dataset
-        https://www.kaggle.com/datasets/valakhorasani/mobile-device-usage-and-user-behavior-dataset?resource=download
-        This dataset provides a comprehensive analysis of mobile device usage
+      <h1>User Behavior Dataset</h1>
+      <p>This dataset provides a comprehensive analysis of mobile device usage
         patterns and user behavior classification. It contains 700 samples of
         user data, including metrics such as app usage time, screen-on time,
         battery drain, and data consumption. Each entry is categorized into one
         of five user behavior classes, ranging from light to extreme usage,
-        allowing for insightful analysis and modeling. Key Features: - User ID:
-        Unique identifier for each user. - Device Model: Model of the user's
-        smartphone. - Operating System: The OS of the device (iOS or Android). -
-        App Usage Time: Daily time spent on mobile applications, measured in
-        minutes. - Screen On Time: Average hours per day the screen is active. -
-        Battery Drain: Daily battery consumption in mAh. - Number of Apps
-        Installed: Total apps available on the device. - Data Usage: Daily
-        mobile data consumption in megabytes. - Age: Age of the user. - Gender:
-        Gender of the user (Male or Female). - User Behavior Class:
-        Classification of user behavior based on usage patterns (1 to 5).
+        allowing for insightful analysis and modeling.</p>
+        <h1>Key Features:</h1>
+        <ul>
+          <li>User ID: Unique identifier for each user.</li>
+          <li>Device Model: Model of the user's smartphone.</li>
+          <li>Operating System: The OS of the device (iOS or Android).</li>
+          <li>App Usage Time: Daily time spent on mobile applications, measured in minutes.</li>
+          <li>Screen On Time: Average hours per day the screen is active.</li>
+          <li>Battery Drain: Daily battery consumption in mAh.</li>
+          <li>Number of Apps Installed: Total apps available on the device. </li>
+          <li>Data Usage: Daily mobile data consumption in megabytes.</li>
+          <li>Age: Age of the user.</li>
+          <li>Gender:Gender of the user (Male or Female).</li>
+          <li>User Behavior Class: Classification of user behavior based on usage patterns (1 to 5).</li>
+        </ul>
+      <p>
+        <a href="https://www.kaggle.com/datasets/valakhorasani/mobile-device-usage-and-user-behavior-dataset?resource=download">Sourced from this Kaggle Dataset</a>
       </p>
     </div>
   );
